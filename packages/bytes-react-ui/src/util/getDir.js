c6dad67a13b54eba81455f49a3844a2988063e8f
ShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-09-26 01:19:07 +0800

    feat(page of detail and header): 添加了 detail页面以及演示用的header (#2)
    Co-authored-by: wantedsr <2426756650@qq.com>

---
+++ packages/bytes-react-ui/src/util/getDir.js
@@ -0,0 +1,18 @@
+const fs = require('fs')
+const path = require('path')
+
+let p = __dirname
+
+const getFile = async path => {
+  let fileList = []
+  await fs.readdirSync(path)
+}
+
+getFile(p).then(res => {
+  console.log(res)
+})
+
+module.exports = {
+  __dirname,
+  getFile,
+}
