f7a0b185f3f7183376e27563c23778f85c2100fd
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-09-25 22:24:34 +0800

    fix code format

--- packages/bytes-react-cli/src/server/controller/project.js
+++ packages/bytes-react-cli/src/server/controller/project.js
@@ -4,7 +4,7 @@ const { PROJECT_STATUS_WAITING } = require('../lib/define')
 /**
  *  @description 获取创建项目的状态
  */
-const getProjectStatus = async (ctx, next) => {
+const getProjectStatus = async (ctx, _next) => {
   sndResponse(ctx.response)(global.createProjectStatus)
   global.createProjectStatus = PROJECT_STATUS_WAITING
 }
