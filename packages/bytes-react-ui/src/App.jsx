540cfd4e1a6b53d565ecaf698f262da7cb00921e
Zhu Zihang <colored_black@outlook.com>
2021-09-25 17:48:58 +0800

    [fix] add App.js router example

--- packages/bytes-react-ui/src/App.jsx
+++ packages/bytes-react-ui/src/App.jsx
@@ -4,15 +4,18 @@ import routes from './routes'
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        {routes.map(route => (
-          <Route exact key={route.path} path={route.path}>
-            <route.component />
-          </Route>
-        ))}
-      </Switch>
-    </Router>
+    <>
+      <div>Hello</div>
+      <Router>
+        <Switch>
+          {routes.map(route => (
+            <Route exact key={route.path} path={route.path}>
+              <route.component />
+            </Route>
+          ))}
+        </Switch>
+      </Router>  
+    </>
   )
 }
 
