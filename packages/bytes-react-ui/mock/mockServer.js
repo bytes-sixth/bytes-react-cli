2ba0b10d31a424f8a194b9bdd80df743c9dda8e0
Zhu Zihang <colored_black@outlook.com>
2021-09-26 11:23:05 +0800

    [add] add a mock server that can provide test data, use 'yarn mock' to create it

--- packages/bytes-react-ui/mock/mockServer.js
+++ packages/bytes-react-ui/mock/mockServer.js
@@ -1,8 +1,19 @@
-let Koa = require('koa')
+const Koa = require('koa')
+const Router = require('koa-router')
+const config = require('../config/config.dev')
+
+const mockUrl = config.mockServer.host + ':' + config.mockServer.ports[0]
+
 const app = new Koa()
+let routerHome = new Router()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
+let router = new Router()
+
+routerHome.get('/hello', async ctx => {
+  ctx.body = 'hello'
 })
+router.use('/api', routerHome.routes(), routerHome.allowedMethods())
 
-app.listen(8001)
+app.use(router.routes()).use(router.allowedMethods())
+
+app.listen(config.mockServer.ports[0])
