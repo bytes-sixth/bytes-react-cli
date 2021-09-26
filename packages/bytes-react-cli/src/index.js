ead5eb4e694a083c1305428754d250dfe210df03
XYShaoKang <38753204+XYShaoKang@users.noreply.github.com>
2021-09-26 13:45:39 +0800

    refactor(bytes-react-cli): cli index

--- packages/bytes-react-cli/src/index.js
+++ packages/bytes-react-cli/src/index.js
@@ -2,18 +2,16 @@ const { program } = require('commander')
 const create = require('./command/create')
 const ui = require('./command/ui')
 
-;(async function () {
-  program.version('0.0.1')
+program.version('0.0.1')
 
-  program
-    .command('create')
-    .argument('<app-name>', '要创建的项目名称')
-    .option('-i --install-dep', '是否需要安装依赖')
-    .option('--use-yarn', '使用 yarn 作为包管理器,默认使用 npm')
-    .description('创建一个新的 React 项目')
-    .action(create)
+program
+  .command('create')
+  .argument('<app-name>', '要创建的项目名称')
+  .option('-i --install-dep', '是否需要安装依赖')
+  .option('--use-yarn', '使用 yarn 作为包管理器,默认使用 npm')
+  .description('创建一个新的 React 项目')
+  .action(create)
 
-  program.command('ui').description('使用 UI 创建 React 项目').action(ui)
+program.command('ui').description('使用 UI 创建 React 项目').action(ui)
 
-  await program.parseAsync(process.argv)
-})()
+program.parseAsync(process.argv)
